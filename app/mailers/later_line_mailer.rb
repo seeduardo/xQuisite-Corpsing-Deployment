7b831ebe70a809809f57dd7019dc36b2c9ff2d2e
seeduardo <romenflatiron@gmail.com>
2019-02-12 12:38:30 +0000

    Composed html and text versions of finished_poem_email

--- app/mailers/later_line_mailer.rb
+++ app/mailers/later_line_mailer.rb
@@ -1,2 +1,9 @@
 class LaterLineMailer < ApplicationMailer
+
+  def finished_poem_email(first_line, current_line)
+    @first_line = first_line
+    @current_line = current_line
+    mail(to: @current_line.email, subject: 'Your x-Quisite Corpse is now ready')
+  end
+
 end
